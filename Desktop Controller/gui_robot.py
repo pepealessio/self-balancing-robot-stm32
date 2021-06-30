170fdd0fe3b5e31e7bf8c9839734b3d16f7b515b
pmns5 <p.mansi5@studenti.unisa.it>
2021-06-30 12:07:42 +0200

    diagonal buttons deleted

--- Desktop Controller/gui_robot.py
+++ Desktop Controller/gui_robot.py
@@ -12,10 +12,10 @@ bool_up = True
 bool_down = True
 bool_left = True
 bool_right = True
-bool_up_right = True
-bool_down_right = True
-bool_up_left = True
-bool_down_left = True
+# bool_up_right = True
+# bool_down_right = True
+# bool_up_left = True
+# bool_down_left = True
 bool_buz = True
 
 
@@ -130,118 +130,118 @@ def right_released():
         except OSError:
             disconnect_handle()
 
-
-def up_left_pressed():
-    """ Sends control byte with the up and left bits set through the Bluetooth Socket  """
-    upLeftBtn['background'] = 'LightGoldenrod3'
-    global bool_up_left
-    if bool_up_left:
-        try:
-            global control_byte
-            control_byte |= 0x50    # 0101 0000
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_up_left = False
-        except OSError:
-            disconnect_handle()
-
-
-def up_left_released():
-    """ Sends control byte with the up and left Bits unset through the Bluetooth Socket  """
-    upLeftBtn['background'] = 'white'
-    global bool_up_left
-    if not bool_up_left:
-        try:
-            global control_byte
-            control_byte &= 0xAF    # 1010 1111
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_up_left = True
-        except OSError:
-            disconnect_handle()
-
-
-def up_right_pressed():
-    """ Sends control byte with the up and right bits set through the Bluetooth Socket  """
-    upRightBtn['background'] = 'LightGoldenrod3'
-    global bool_up_right
-    if bool_up_right:
-        try:
-            global control_byte
-            control_byte |= 0x48    # 0100 1000
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_up_right = False
-        except OSError:
-            disconnect_handle()
-
-
-def up_right_released():
-    """ Sends control byte with the up and right Bits unset through the Bluetooth Socket  """
-    upRightBtn['background'] = 'white'
-    global bool_up_right
-    if not bool_up_right:
-        try:
-            global control_byte
-            control_byte &= 0xB7    # 1011 0111
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_up_right = True
-        except OSError:
-            disconnect_handle()
-
-
-def down_left_pressed():
-    """ Sends control byte with the down and left bits set through the Bluetooth Socket  """
-    downLeftBtn['background'] = 'LightGoldenrod3'
-    global bool_down_left
-    if bool_down_left:
-        try:
-            global control_byte
-            control_byte |= 0x30    # 0011 0000
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_down_left = False
-        except OSError:
-            disconnect_handle()
-
-
-def down_left_released():
-    """ Sends control byte with the down and left Bits unset through the Bluetooth Socket  """
-    downLeftBtn['background'] = 'white'
-    global bool_down_left
-    if not bool_down_left:
-        try:
-            global control_byte
-            control_byte &= 0xCF    # 1100 1111
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_down_left = True
-        except OSError:
-            disconnect_handle()
-
-
-def down_right_pressed():
-    """ Sends control byte with the down and right bits set through the Bluetooth Socket  """
-    downRightBtn['background'] = 'LightGoldenrod3'
-    global bool_down_right
-    if bool_down_right:
-        try:
-            global control_byte
-            control_byte |= 0x28    # 0010 1000
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_down_right = False
-        except OSError:
-            disconnect_handle()
-
-
-def down_right_released():
-    """ Sends control byte with the down and right Bits unset through the Bluetooth Socket  """
-    downRightBtn['background'] = 'white'
-    global bool_down_right
-    if not bool_down_right:
-        try:
-            global control_byte
-            control_byte &= 0xD7    # 1101 0111
-            sock.send(control_byte.to_bytes(1, 'big'))
-            bool_down_right = True
-        except OSError:
-            disconnect_handle()
-
+#
+# def up_left_pressed():
+#     """ Sends control byte with the up and left bits set through the Bluetooth Socket  """
+#     upLeftBtn['background'] = 'LightGoldenrod3'
+#     global bool_up_left
+#     if bool_up_left:
+#         try:
+#             global control_byte
+#             control_byte |= 0x50    # 0101 0000
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_up_left = False
+#         except OSError:
+#             disconnect_handle()
+#
+#
+# def up_left_released():
+#     """ Sends control byte with the up and left Bits unset through the Bluetooth Socket  """
+#     upLeftBtn['background'] = 'white'
+#     global bool_up_left
+#     if not bool_up_left:
+#         try:
+#             global control_byte
+#             control_byte &= 0xAF    # 1010 1111
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_up_left = True
+#         except OSError:
+#             disconnect_handle()
+#
+#
+# def up_right_pressed():
+#     """ Sends control byte with the up and right bits set through the Bluetooth Socket  """
+#     upRightBtn['background'] = 'LightGoldenrod3'
+#     global bool_up_right
+#     if bool_up_right:
+#         try:
+#             global control_byte
+#             control_byte |= 0x48    # 0100 1000
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_up_right = False
+#         except OSError:
+#             disconnect_handle()
+#
+#
+# def up_right_released():
+#     """ Sends control byte with the up and right Bits unset through the Bluetooth Socket  """
+#     upRightBtn['background'] = 'white'
+#     global bool_up_right
+#     if not bool_up_right:
+#         try:
+#             global control_byte
+#             control_byte &= 0xB7    # 1011 0111
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_up_right = True
+#         except OSError:
+#             disconnect_handle()
+#
+#
+# def down_left_pressed():
+#     """ Sends control byte with the down and left bits set through the Bluetooth Socket  """
+#     downLeftBtn['background'] = 'LightGoldenrod3'
+#     global bool_down_left
+#     if bool_down_left:
+#         try:
+#             global control_byte
+#             control_byte |= 0x30    # 0011 0000
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_down_left = False
+#         except OSError:
+#             disconnect_handle()
+#
+#
+# def down_left_released():
+#     """ Sends control byte with the down and left Bits unset through the Bluetooth Socket  """
+#     downLeftBtn['background'] = 'white'
+#     global bool_down_left
+#     if not bool_down_left:
+#         try:
+#             global control_byte
+#             control_byte &= 0xCF    # 1100 1111
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_down_left = True
+#         except OSError:
+#             disconnect_handle()
+#
+#
+# def down_right_pressed():
+#     """ Sends control byte with the down and right bits set through the Bluetooth Socket  """
+#     downRightBtn['background'] = 'LightGoldenrod3'
+#     global bool_down_right
+#     if bool_down_right:
+#         try:
+#             global control_byte
+#             control_byte |= 0x28    # 0010 1000
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_down_right = False
+#         except OSError:
+#             disconnect_handle()
+#
+#
+# def down_right_released():
+#     """ Sends control byte with the down and right Bits unset through the Bluetooth Socket  """
+#     downRightBtn['background'] = 'white'
+#     global bool_down_right
+#     if not bool_down_right:
+#         try:
+#             global control_byte
+#             control_byte &= 0xD7    # 1101 0111
+#             sock.send(control_byte.to_bytes(1, 'big'))
+#             bool_down_right = True
+#         except OSError:
+#             disconnect_handle()
+#
 
 def buz_pressed():
     """ Sends control byte with the buz bit set through the Bluetooth Socket  """
@@ -300,10 +300,10 @@ def disable_all():
     downBtn["state"] = "disabled"
     leftBtn["state"] = "disabled"
     rightBtn["state"] = "disabled"
-    upLeftBtn["state"] = "disabled"
-    downLeftBtn["state"] = "disabled"
-    upRightBtn["state"] = "disabled"
-    downRightBtn["state"] = "disabled"
+    # upLeftBtn["state"] = "disabled"
+    # downLeftBtn["state"] = "disabled"
+    # upRightBtn["state"] = "disabled"
+    # downRightBtn["state"] = "disabled"
     buzBtn["state"] = "disabled"
 
 
@@ -314,10 +314,10 @@ def active_all():
     downBtn["state"] = "normal"
     leftBtn["state"] = "normal"
     rightBtn["state"] = "normal"
-    upLeftBtn["state"] = "normal"
-    downLeftBtn["state"] = "normal"
-    upRightBtn["state"] = "normal"
-    downRightBtn["state"] = "normal"
+    # upLeftBtn["state"] = "normal"
+    # downLeftBtn["state"] = "normal"
+    # upRightBtn["state"] = "normal"
+    # downRightBtn["state"] = "normal"
     buzBtn["state"] = "normal"
 
 
@@ -366,7 +366,7 @@ def on_leave(btn):
 
 # configure workspace
 ws = Tk()
-ws.title("SBL controller")
+ws.title("SBR controller")
 ws.geometry('470x300')
 ws.configure(bg="lightgray")
 
@@ -376,10 +376,10 @@ upBtn = Button(ws, text="🡡 (W)", bg='white', font=buttonFont)
 downBtn = Button(ws, text="🡣 (S)", bg='white', font=buttonFont)
 leftBtn = Button(ws, text="🡠 (A)", bg='white', font=buttonFont)
 rightBtn = Button(ws, text="🡢 (D)", bg='white', font=buttonFont)
-upLeftBtn = Button(ws, text="🡤 (WA)", bg='white', font=buttonFont)
-downLeftBtn = Button(ws, text="🡧 (SA)", bg='white', font=buttonFont)
-upRightBtn = Button(ws, text="🡥 (WD)", bg='white', font=buttonFont)
-downRightBtn = Button(ws, text="🡦 (SD)", bg='white', font=buttonFont)
+# upLeftBtn = Button(ws, text="🡤 (WA)", bg='white', font=buttonFont)
+# downLeftBtn = Button(ws, text="🡧 (SA)", bg='white', font=buttonFont)
+# upRightBtn = Button(ws, text="🡥 (WD)", bg='white', font=buttonFont)
+# downRightBtn = Button(ws, text="🡦 (SD)", bg='white', font=buttonFont)
 buzBtn = Button(ws, text="Horn (ws)", bg='white', font=buttonFont)
 connectBtn = Button(ws, text="Connect", bg='gray', fg="white", command=connect, width=10, font=buttonFont)
 connectedLbl = Label(ws, text="NOT connected!", bg='red', fg='white', font=buttonFont)
@@ -404,26 +404,26 @@ rightBtn.bind('<ButtonRelease>', lambda event: right_released())
 rightBtn.bind('<ButtonPress>', lambda event: right_pressed())
 rightBtn.bind('<Enter>', lambda event: on_enter(rightBtn))
 rightBtn.bind('<Leave>', lambda event: on_leave(rightBtn))
-
-upLeftBtn.bind('<ButtonRelease>', lambda event: up_left_released())
-upLeftBtn.bind('<ButtonPress>', lambda event: up_left_pressed())
-upLeftBtn.bind('<Enter>', lambda event: on_enter(upLeftBtn))
-upLeftBtn.bind('<Leave>', lambda event: on_leave(upLeftBtn))
-
-upRightBtn.bind('<ButtonRelease>', lambda event: up_right_released())
-upRightBtn.bind('<ButtonPress>', lambda event: up_right_pressed())
-upRightBtn.bind('<Enter>', lambda event: on_enter(upRightBtn))
-upRightBtn.bind('<Leave>', lambda event: on_leave(upRightBtn))
-
-downLeftBtn.bind('<ButtonRelease>', lambda event: down_left_released())
-downLeftBtn.bind('<ButtonPress>', lambda event: down_left_pressed())
-downLeftBtn.bind('<Enter>', lambda event: on_enter(downLeftBtn))
-downLeftBtn.bind('<Leave>', lambda event: on_leave(downLeftBtn))
-
-downRightBtn.bind('<ButtonRelease>', lambda event: down_right_released())
-downRightBtn.bind('<ButtonPress>', lambda event: down_right_pressed())
-downRightBtn.bind('<Enter>', lambda event: on_enter(downRightBtn))
-downRightBtn.bind('<Leave>', lambda event: on_leave(downRightBtn))
+#
+# upLeftBtn.bind('<ButtonRelease>', lambda event: up_left_released())
+# upLeftBtn.bind('<ButtonPress>', lambda event: up_left_pressed())
+# upLeftBtn.bind('<Enter>', lambda event: on_enter(upLeftBtn))
+# upLeftBtn.bind('<Leave>', lambda event: on_leave(upLeftBtn))
+#
+# upRightBtn.bind('<ButtonRelease>', lambda event: up_right_released())
+# upRightBtn.bind('<ButtonPress>', lambda event: up_right_pressed())
+# upRightBtn.bind('<Enter>', lambda event: on_enter(upRightBtn))
+# upRightBtn.bind('<Leave>', lambda event: on_leave(upRightBtn))
+#
+# downLeftBtn.bind('<ButtonRelease>', lambda event: down_left_released())
+# downLeftBtn.bind('<ButtonPress>', lambda event: down_left_pressed())
+# downLeftBtn.bind('<Enter>', lambda event: on_enter(downLeftBtn))
+# downLeftBtn.bind('<Leave>', lambda event: on_leave(downLeftBtn))
+#
+# downRightBtn.bind('<ButtonRelease>', lambda event: down_right_released())
+# downRightBtn.bind('<ButtonPress>', lambda event: down_right_pressed())
+# downRightBtn.bind('<Enter>', lambda event: on_enter(downRightBtn))
+# downRightBtn.bind('<Leave>', lambda event: on_leave(downRightBtn))
 
 buzBtn.bind('<ButtonRelease>', lambda event: buz_released())
 buzBtn.bind('<ButtonPress>', lambda event: buz_pressed())
@@ -436,10 +436,10 @@ upBtn.grid(row=0, column=1, sticky=NSEW, padx=1, pady=1)
 downBtn.grid(row=2, column=1, sticky=NSEW, padx=1, pady=1)
 leftBtn.grid(row=1, column=0, sticky=NSEW, padx=1, pady=1)
 rightBtn.grid(row=1, column=2, sticky=NSEW, padx=1, pady=1)
-upLeftBtn.grid(row=0, column=0, sticky=NSEW, padx=1, pady=1)
-upRightBtn.grid(row=0, column=2, sticky=NSEW, padx=1, pady=1)
-downLeftBtn.grid(row=2, column=0, sticky=NSEW, padx=1, pady=1)
-downRightBtn.grid(row=2, column=2, sticky=NSEW, padx=1, pady=1)
+# upLeftBtn.grid(row=0, column=0, sticky=NSEW, padx=1, pady=1)
+# upRightBtn.grid(row=0, column=2, sticky=NSEW, padx=1, pady=1)
+# downLeftBtn.grid(row=2, column=0, sticky=NSEW, padx=1, pady=1)
+# downRightBtn.grid(row=2, column=2, sticky=NSEW, padx=1, pady=1)
 buzBtn.grid(row=1, column=1, sticky=NSEW, padx=1, pady=1)
 connectBtn.grid(row=0, column=3, sticky=NSEW, padx=1, pady=1)
 connectedLbl.grid(row=1, column=3, rowspan=2, sticky=NSEW, padx=1, pady=1)
@@ -449,18 +449,17 @@ for x in range(3):
     ws.rowconfigure(x, weight=1)
 
 # Socket connection
-# target_name = "SBLGruppo2"
-# targetBluetoothMacAddress = None
-# devices = bluetooth.discover_devices()
-# print(devices)
-# for bdaddr in devices:
-#     # print(bluetooth.lookup_name(bdaddr))
-#     if target_name == bluetooth.lookup_name(bdaddr):
-#         targetBluetoothMacAddress = bdaddr
-#         print(targetBluetoothMacAddress)
-#         break
+target_name = "SBLGruppo2"
+targetBluetoothMacAddress = None
+devices = bluetooth.discover_devices()
+for bdaddr in devices:
+    # print(bluetooth.lookup_name(bdaddr))
+    if target_name == bluetooth.lookup_name(bdaddr):
+        targetBluetoothMacAddress = bdaddr
+        print(targetBluetoothMacAddress)
+        break
 
-targetBluetoothMacAddress = '24:6F:28:B4:F0:6E'
+# targetBluetoothMacAddress = '24:6F:28:B4:F0:6E'
 if targetBluetoothMacAddress is None:
     exit(2)
 
